032db69e37ff0d9c5077dd2229f203cea8d2bb39
fairka <fairman766@gmail.com>
2022-12-03 03:18:54 -0800

    refactored day three

--- day_three/day_three.py
+++ day_three/day_three.py
@@ -1,39 +1,49 @@
-list = []
-total = 0
-temp1 = []
-temp2 = []
-temp3 = []
+def main():
+    part1()
+    part2()
+    
 
-with open('input.txt', 'r') as f:
-    sack = [line.strip() for line in f]
-    #print(sack)
+def part1():
+    total = 0
 
-    for i in range(len(sack)):
-        if (i+1) % 3 == 0:
-            temp1 = [x for x in sack[i-2]]
-            temp2 = [x for x in sack[i-1]]
-            temp3= [x for x in sack[i]]
-            for char in temp1:
-                if char in temp2 and char in temp3:
+    with open('input.txt', 'r') as f:
+        sack = [line.strip() for line in f]
+        for rucksack in sack:
+            temp = [x for x in rucksack]
+            A = temp[:len(temp) // 2]
+            B = temp[len(temp) // 2:]
+
+        
+            for char in A:
+                if char in B:
                     if char.isupper():
                         total += ord(char) - 38
                     else:
                         total += ord(char) - 96
                     break
+    print (total)
 
-        
-        #print(temp1)
-        # A = temp[:len(temp) // 2]
-        # B = temp[len(temp) // 2:]
+def part2():
+    total = 0
+    temp1 = []
+    temp2 = []
+    temp3 = []
+    with open('input.txt', 'r') as f:
+        sack = [line.strip() for line in f]
 
-    
-#         for char in A:
-#             if char in B:
-#                 print(char)
-#                 if char.isupper():
-#                     total += ord(char) - 38
-#                 else:
-#                     total += ord(char) - 96
-#                 break
-print(total)        
-        
+        for i in range(len(sack)):
+            if (i+1) % 3 == 0:
+                temp1 = [x for x in sack[i-2]]
+                temp2 = [x for x in sack[i-1]]
+                temp3= [x for x in sack[i]]
+                for char in temp1:
+                    if char in temp2 and char in temp3:
+                        if char.isupper():
+                            total += ord(char) - 38
+                        else:
+                            total += ord(char) - 96
+                        break
+    print(total) 
+
+if __name__ == '__main__':
+    main()
