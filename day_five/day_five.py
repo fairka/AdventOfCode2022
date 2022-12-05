7b8a145b3c840649bd9cc11fda157e7eb96c4511
fairka <fairman766@gmail.com>
2022-12-04 22:52:15 -0800

    day 5

---
+++ day_five/day_five.py
@@ -0,0 +1,72 @@
+def main():
+    #partone()
+    parttwo()
+
+
+
+def partone():
+    stacks = []
+    pairTxt = open('input.txt', 'r')
+    listInstructions = [line.strip("\n") for line in pairTxt]
+    finalIndex = 0
+
+    for i in range(len(listInstructions)):
+        stacks.append([])
+        if listInstructions[i] == "":
+            finalIndex = i - 1
+            stacks.pop()
+            break
+
+    for i in range(finalIndex, -1, -1):
+        counter = 0
+        temp = listInstructions[i]
+
+        for j in range(len(temp)):
+            if (j+1) % 4 == 0:
+                counter +=1
+            if temp[j].isalpha():
+                    stacks[counter].append(temp[j])
+
+    for i in range(finalIndex+2, len(listInstructions)):
+        temp = listInstructions[i].split()
+        for j in range(int(temp[1])):
+            box = stacks[int(temp[3])-1].pop()
+            stacks[int(temp[5])-1].append(box)
+    print(stacks)
+        
+
+                    
+                     
+   
+
+def parttwo():
+    stacks = []
+    pairTxt = open('input.txt', 'r')
+    listInstructions = [line.strip("\n") for line in pairTxt]
+    finalIndex = 0
+
+    for i in range(len(listInstructions)):
+        stacks.append([])
+        if listInstructions[i] == "":
+            finalIndex = i - 1
+            stacks.pop()
+            break
+
+    for i in range(finalIndex, -1, -1):
+        counter = 0
+        temp = listInstructions[i]
+        for j in range(len(temp)):
+            if (j+1) % 4 == 0:
+                counter +=1
+            if temp[j].isalpha():
+                    stacks[counter].append(temp[j])
+
+    for i in range(finalIndex+2, len(listInstructions)):
+        temp = listInstructions[i].split()
+        for j in range(int(temp[1])-1, -1, -1):
+            box = stacks[int(temp[3])-1].pop(len(stacks[int(temp[3])-1])-j-1)
+            stacks[int(temp[5])-1].append(box)
+    print(stacks)
+
+if __name__ == '__main__':
+    main()
